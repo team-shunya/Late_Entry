044241ff0c58d1c1cdca12006f5a591e7230d339
pikachuPokemon <akshay.kmr4321@gmail.com>
2016-03-10 01:10:36 +0530

    commit

--- server/main.js
+++ server/main.js
@@ -1,5 +1,24 @@
 Meteor.startup(function(){
 	defaultTemplate='a';
 	TestRunning=false;
+	var testing=true;
+	if(testing){
+		Tests.remove({});
+
+		var test= {
+			templateData: [{name: 'a', weight: 5,successCriteria: 0.8},
+			{name: 'c', weight: 2,successCriteria: 0.8},
+			{name: 'b', weight: 3,successCriteria: 0.8}],
+			components: [{type: 'id', name: 'feat0'},{type: 'id', name: 'feat1'}],
+			duration: 30
+		};
+		Meteor.call('createTest',test,function(err,result){
+			if(err)
+				console.log(err);
+			else{
+
+			}
+		});		
+	}
 });
 
--- server/methods.js
+++ server/methods.js
@@ -1,9 +1,91 @@
+var counter=0;
+
+
 Meteor.methods({
 	createSession: function(){
-		if(TestRunning===false)
+		if(TestRunning===false){
 			return {
-				template: 'a'
+				templateName: defaultTemplate
 			}
+		}else{
+			var test= Tests.findOne({status:'running'});
+
+			var templates= Templates.find({testId: test._id}).fetch();
+			//logic
+
+			templates.sort(function(a,b){
+				return a.currentWeight>b.currentWeight;
+			});
+
+			function resetWeight(){
+				templates.forEach(function(t){
+					Templates.update({_id: t._id},{$set: {currentWeight: t.weight}});
+				})
+			}
+
+			var templateCount= templates.length;
+			while(templates[counter].currentWeight==0){
+				counter=(counter+1)%templateCount;
+				if(templates[0].currentWeight===0){
+					resetWeight();
+				}
+			}
+			var updatedTemplateName=templates[counter].name;
+			Templates.update({_id:templates[counter]._id},{$set:{currentWeight:templates[counter].currentWeight-1},$inc: {currentUsers: 1}});
+			counter=(counter+1)%templateCount;
+
+
+			var sessionCredentials= {
+				testId: test._id,
+				sessionId: Meteor.random(),
+				expireTime: test.endTime,
+				templateName: updatedTemplateName, 
+				testRunning: true
+			};
+
+			Session.insert(sessionCredentials);
+
+			return sessionCredentials;			
+		}
 	},
+	createTest: function(testData){
+
+		/*
+			{
+				templateData: [{name, weight,successCriteria}]
+				[component],
+				duration: minutes
+			}
+
+		*/
+
+		var test= {
+			status: 'running',
+			startTime: new Date(),
+			endTime: moment().add(testData.duration,'minutes').toDate(),
+			components: testData.components
+		};
+
+		var testId=Tests.insert(test);
+
+
+		var idList=[];
+		testData.templateData.forEach(function(t){
+			var id=Templates.insert({
+				name: t.name,
+				weight: t.weight,
+				successCriteria: t.successCriteria,
+				testId:testId,
+
+				currentWeight: t.weight,
+				score: 0,
+				currentUsers: 0,
+				success: false
+			});
+			idList.push(id);
+		});
+		Tests.update({_id:testId},{$set:{templates:idList}});
+	
+	}
 
 });
